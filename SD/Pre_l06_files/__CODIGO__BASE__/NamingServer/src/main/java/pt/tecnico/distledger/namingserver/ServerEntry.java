da6b512420f9bd25542c2e6248bdd3007ed72553
JoseCutileiro <jos3ndo@gmail.com>
2023-03-23 22:30:01 +0000

    fix

--- SD/Pre_l06_files/__CODIGO__BASE__/NamingServer/src/main/java/pt/tecnico/distledger/namingserver/ServerEntry.java
+++
@@ -1,6 +0,0 @@
-// LAB 5
-
-public class ServerEntry {
-    // Infromação de TODOS os servidores
-    // host:port e qualificador
-}
--- SD/Pre_l06_files/__CODIGO__BASE__/NamingServer/src/main/java/pt/tecnico/distledger/namingserver/ServiceEntry.java
+++
@@ -1,6 +0,0 @@
-// LAB 5
-
-public class ServicEntry {
-    // NOME DE UM SERVIÇO
-    // TODOS OS SERVERE ENTRYES
-}
